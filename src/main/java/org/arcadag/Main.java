5f906023d163c8a38dd758c33571076ad23a21b7
geekbrains <piknik0711@yandex.ru>
2023-01-13 14:29:08 +0300

    Initial commit

--- src/main/java/org/arcadag/Main.java
+++ src/main/java/org/arcadag/Main.java
@@ -1,4 +1,6 @@
-package org.arcadag;public class Main {
+package org.arcadag;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
